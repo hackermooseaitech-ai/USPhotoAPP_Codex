be5c8e01c23c1a8bd689c8e94feb761e89d4e99a
James Chen <james.pochih.chen@gmail.com>
2026-05-13 14:26:23 -0600

    Show social login account details

--- orders/views.py
+++ orders/views.py
@@ -18,6 +18,14 @@ from .services.photo_processor import process_order_photo
 
 def index(request):
     providers = settings.SOCIALACCOUNT_PROVIDERS
+    social_login = None
+    if request.user.is_authenticated:
+        account = request.user.socialaccount_set.order_by("-id").first()
+        if account:
+            social_login = {
+                "provider": account.get_provider().name,
+                "email": account.extra_data.get("email") or request.user.email or request.user.username,
+            }
     return render(
         request,
         "orders/index.html",
@@ -25,6 +33,7 @@ def index(request):
             "form": PhotoUploadForm(),
             "google_oauth_ready": bool(providers.get("google", {}).get("APPS")),
             "yahoo_oauth_ready": bool(providers.get("yahoo", {}).get("APPS")),
+            "social_login": social_login,
         },
     )
 
